48e363f5782662fc29a317973cc0a52db4b35971
Steven Hazel <sah@saucelabs.com>
2011-11-07 09:32:15 -0600

    readline ftw

--- esh.rb
+++ esh.rb
@@ -1,27 +1,42 @@
+require "readline"
+
+stty_save = `stty -g`.chomp
+trap("INT") { system "stty", stty_save; exit }
+#trap("INT", "SIG_IGN")
 scope = Proc.new {}
 
-while true
-  "#{Dir.pwd}> ".display
-  gets.each do |l|
-    l = l.chomp
-    begin
-      if l.match /^cd$/
-        Dir.chdir
-      elsif l.match /^cd /
-        d = l[3, l.length]
-        d = File.expand_path(d)
-        p d
-        Dir.chdir d
-      else
-        puts(eval(l, scope.binding))
-      end
-    rescue SyntaxError, NameError => e
-      l = "\"" + l.split(" ").join("\" + \" ") + "\""
-      l = eval(l)
-      puts `#{l}`
-    rescue StandardError => e
-      puts "FAIL"
-      p e
+Readline.completion_proc = Proc.new do |s|
+  (methods+Dir[s+'*']).grep(/^#{Regexp.escape(s)}/)
+end
+
+while l = Readline.readline("#{Dir.pwd}$ ", true)
+
+  Readline::HISTORY.pop if /^\s*$/ =~ l
+
+  begin
+    if Readline::HISTORY[Readline::HISTORY.length-2] == l
+      Readline::HISTORY.pop
     end
+  rescue IndexError
+  end
+
+  begin
+    if l.match /^cd$/
+      Dir.chdir
+    elsif l.match /^cd /
+      d = l[3, l.length]
+      d = File.expand_path(d)
+      p d
+      Dir.chdir d
+    else
+      puts(eval(l, scope.binding))
+    end
+  rescue SyntaxError, NameError => e
+    l = "\"" + l.split(" ").join("\" + \" ") + "\""
+    l = eval(l)
+    puts `#{l}`
+  rescue StandardError => e
+    puts "FAIL"
+    p e
   end
 end
